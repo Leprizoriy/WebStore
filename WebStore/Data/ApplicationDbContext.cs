cdfe2a84805c0f0a9c53bed0f3a2328a0b9fb414
Leprizoriy <leprizoriy@outlook.com>
2023-05-02 21:48:25 +0300

    add EF migration to create the db

---
+++ WebStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
