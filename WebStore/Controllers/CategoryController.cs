96cfb464ba4ad2e16df7b707d98ee817a943c5b1
Leprizoriy <leprizoriy@outlook.com>
2023-05-05 16:42:06 +0300

    add toastr notification

--- WebStore/Controllers/CategoryController.cs
+++ WebStore/Controllers/CategoryController.cs
@@ -35,6 +35,7 @@ namespace WebStore.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
 
@@ -64,6 +65,7 @@ namespace WebStore.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
 
@@ -97,6 +99,7 @@ namespace WebStore.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
