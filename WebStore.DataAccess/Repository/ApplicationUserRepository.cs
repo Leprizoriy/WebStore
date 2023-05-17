87216d54d9d26ac4033865b05614b8358c19e876
Leprizoriy <leprizoriy@outlook.com>
2023-05-17 16:32:31 +0300

    add UnitOfWork for ApplicationUser and ShoppingCart

---
+++ WebStore.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.DataAccess.Data;
+using WebStore.DataAccess.Repository.IRepository;
+using WebStore.Models;
+
+namespace WebStore.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
