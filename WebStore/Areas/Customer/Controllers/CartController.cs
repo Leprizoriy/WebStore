fcf996fffc1a568b05544643edb2b90a96b03d6a
Leprizoriy <leprizoriy@outlook.com>
2023-05-17 19:10:05 +0300

    add shopping cart view

---
+++ WebStore/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
