e82cf43732c5dde0eae202649b0b6f11157bdfef
Leprizoriy <leprizoriy@outlook.com>
2023-05-18 19:16:59 +0300

    add migration for create OrderDetail and OrderHeader tables

---
+++ WebStore.DataAccess/Repository/IRepository/IOderHeaderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.DataAccess.Repository.IRepository
+{
+    public interface IOderHeaderRepository : IRepository<OrderHeader>
+    {
+        void Update(OrderHeader obj);
+    }
+}
