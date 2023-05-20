96791e0562867df434f26b896edaa69473163fe4
Leprizoriy <leprizoriy@outlook.com>
2023-05-20 19:31:04 +0300

    add order view

--- WebStore/Areas/Admin/Controllers/OrderController.cs
+++ WebStore/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using WebStore.DataAccess.Repository.IRepository;
 using WebStore.Models;
 using WebStore.Utility;
@@ -23,13 +24,32 @@ namespace WebStore.Areas.Admin.Controllers
 			return View();
 		}
 
-		#region API CALLS
+        #region API CALLS
 
-		[HttpGet]
-		public IActionResult GetAll()
+        [HttpGet]
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-			return Json(new { data = objOrderHeaders });
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusPending);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = objOrderHeaders });
 		}
 
 		#endregion
